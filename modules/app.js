ac98ecd71b7b06e917a3ca27e0923e0fbf7870f1
Daniil <kareek.exe@gmail.com>
2017-05-19 16:10:04 +0300

    Изменения внешнего вида, отключена возможность менять стили

--- modules/app.js
+++ modules/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('myApp', ['ngMaterial'])
+
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('cyan')
+    .accentPalette('cyan' )
+    .warnPalette('cyan')
+    //.backgroundPalette('cyan')
+    .dark();
+});
